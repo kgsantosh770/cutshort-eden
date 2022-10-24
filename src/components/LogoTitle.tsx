8356cf27acd183ed6b854508718933bf8fb4c994
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-24 20:25:45 +0530

    logo title ui changes

--- src/components/LogoTitle.tsx
+++ src/components/LogoTitle.tsx
@@ -2,9 +2,9 @@ import Logo from '../assets/images/logo.png';
 
 export default function LogoTitle() {
     return (
-        <div className='flex justify-center items-center'>
-            <img src={Logo} alt="logo" className='inline w-8 aspect-square mr-0.5'/>
-            <span className='text-xl font-semibold'>Eden</span>
+        <div className='flex justify-center items-end mr-2'>
+            <img src={Logo} alt="logo" className='inline w-8 aspect-square mr-2.5'/>
+            <span className='text-2xl font-semibold'>Eden</span>
         </div>
     )
 }
