8ab45e88ee10decf432cc657f5c30149bbec601a
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-24 18:34:34 +0530

    new colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#664DE5',
+      }
+    },
   },
   plugins: [],
 }
