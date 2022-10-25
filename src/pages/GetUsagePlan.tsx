9b95658fb236ec1af60c4e93eed28d4a4233893f
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-25 10:49:48 +0530

    usage plan info page

---
+++ src/pages/GetUsagePlan.tsx
@@ -0,0 +1,44 @@
+import Card from "../components/Card";
+import Form from "../components/FormHOC/Form";
+import SinglePersonIcon from "../assets/images/person.png";
+import TeamIcon from "../assets/images/team.png";
+import { useState } from "react";
+
+export default function GetUsageType() {
+  const [activeCardId, setActiveCardId] = useState(0);
+
+  const CardDetails = [
+    {
+      icon: SinglePersonIcon,
+      title: "For myself",
+      description: "Write better. Think more clearly. Stay organized.",
+    },
+    {
+      icon: TeamIcon,
+      title: "With my team",
+      description: "Wikis, docs, tasks & projects, all in one place.",
+    }
+  ]
+
+  const cards = CardDetails.map((card, index) => 
+  <Card 
+    key={index}
+    icon={card.icon}
+    title={card.title}
+    description={card.description}
+    active={activeCardId === index ? true : false}
+  />
+  )
+
+  return (
+    <Form
+      title="How are you planning to use Eden?"
+      subtitle="We'll streamline your setup experience accordingly."
+      buttonText="Create Workspace"
+    >
+      <div className="flex justify-between">
+        {cards}
+      </div>
+    </Form>
+  )
+}
