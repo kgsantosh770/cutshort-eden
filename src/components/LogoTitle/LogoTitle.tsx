188ff099aff779b47988298e05a58929cde2fe48
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-26 09:57:16 +0530

    spacing changes

--- src/components/LogoTitle/LogoTitle.tsx
+++ src/components/LogoTitle/LogoTitle.tsx
@@ -3,8 +3,8 @@ import Logo from '../../assets/images/logo.png';
 export default function LogoTitle() {
     return (
         <div className='flex justify-center items-end mr-2'>
-            <img src={Logo} alt="logo" className='inline w-8 aspect-square'/>
-            <span className='text-2xl font-semibold leading-snug'>Eden</span>
+            <img src={Logo} alt="logo" className='inline w-8 aspect-square mr-0.5'/>
+            <span className='text-2xl font-semibold leading-[1.1]'>Eden</span>
         </div>
     )
 }
