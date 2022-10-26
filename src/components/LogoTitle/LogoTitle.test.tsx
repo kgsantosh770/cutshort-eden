088a31753355405f37fb136fe89326735132a4bd
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-26 15:22:36 +0530

    logotitle comp made more scalable

--- src/components/LogoTitle/LogoTitle.test.tsx
+++ src/components/LogoTitle/LogoTitle.test.tsx
@@ -1,12 +1,23 @@
 import { render, screen } from '@testing-library/react';
 
 import LogoTitle from "./LogoTitle";
+import Logo from '../../assets/images/logo.png';
 
 describe('LogoTitle', () => {
 
     it('renders properly', () => {
-        render(<LogoTitle />);
-        const logoText = screen.getByText('Eden');
+        const title = "Testing"
+        render(<LogoTitle title={title}/>);
+        const logoText = screen.getByText(title);
         expect(logoText).toBeInTheDocument;
+    })
+
+    it('icon renders only when prop is given', ()=>{
+        render(<LogoTitle title="Testing"/>);
+        let iconElement = screen.queryByRole('img');
+        expect(iconElement).toBeNull;
+        render(<LogoTitle title="Testing" icon={Logo}/>);
+        iconElement = screen.getByRole('img');
+        expect(iconElement).toBeInTheDocument;
     })
 })
