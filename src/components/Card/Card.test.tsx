2e6dabd2d69822bb2a1d01e6dd7f5fad474bc383
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-26 12:50:53 +0530

    basic test cases for card component

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,49 @@
+import {render, screen} from '@testing-library/react';
+import { MouseEvent } from 'react';
+import "@testing-library/jest-dom";
+
+import Card from './Card';
+import PersonIcon from '../../assets/images/person.png';
+
+describe('Card', () => { 
+    const dummyClickEvent = (event: MouseEvent<HTMLButtonElement>) => {}
+    
+    it('renders properly', ()=>{
+        const cardTitle = "testing";
+        render(<Card title={cardTitle} onClick={dummyClickEvent}/>);
+        const cardElement = screen.getByText(cardTitle);
+        expect(cardElement).toBeInTheDocument;
+    })
+
+    it('icon renders only when prop is available', ()=>{
+        render(<Card title="Testing" onClick={dummyClickEvent}/>);
+        let iconElement = screen.queryByRole('img');
+        expect(iconElement).toBeNull;
+        render(<Card title="Testing" icon={PersonIcon} onClick={dummyClickEvent}/>);
+        iconElement = screen.getByRole('img');
+        expect(iconElement).toBeInTheDocument;
+    })
+
+    it('description renders only when prop is available', ()=>{
+        const description = "Test description";
+        render(<Card title="Testing" onClick={dummyClickEvent}/>);
+        let descElement = screen.queryByText(description);
+        expect(descElement).toBeNull;
+        render(<Card title="Testing" description={description} onClick={dummyClickEvent}/>);
+        descElement = screen.getByText(description);
+        expect(descElement).toBeInTheDocument;
+    })
+
+    it('inactive by default',()=>{
+        render(<Card title="Testing" onClick={dummyClickEvent}/>);
+        const cardElement = screen.getByTestId('cardButton');
+        expect(cardElement).toHaveClass('border-gray-200');
+    })
+
+    it('active when active prop is true', ()=>{
+        render(<Card title="Testing" active={true} onClick={dummyClickEvent}/>);
+        const cardElement = screen.getByTestId('cardButton');
+        expect(cardElement).toHaveClass('border-primary');
+    })
+
+})
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,10 +9,11 @@ interface CardProps {
 export default function Card(props: CardProps) {
     return (
         <button 
+            data-testid="cardButton"
             className={`border rounded-md mr-3 lg:w-[11.5rem] py-4 px-3 lg:px-4 text-left ${props.active ? 'border-primary' : 'border-gray-200 grayscale'}`}
             onClick={props.onClick}
         >
-            <img src={props.icon} alt="" className="mb-4"/>
+            {props.icon && <img src={props.icon} alt="" className="mb-4"/>}
             <div className="mb-3 font-semibold">{props.title}</div>
             {props.description && <div className="text-gray-500 mb-2">{props.description}</div>}
         </button>
@@ -20,6 +21,5 @@ export default function Card(props: CardProps) {
 }
 
 Card.defaultProps = {
-    title: 'title',
     active: false,
 }
